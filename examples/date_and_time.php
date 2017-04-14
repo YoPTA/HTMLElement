9d3d3017908b6c67e3f18aaccdaad66ed8bd3ca3
YoPTA <s.nichipurenko@yandex.ru>
2017-04-14 11:49:39 +0300

    Realized DateTimeTime
    1. Реализован класс для работы со временем HTMLTextDateTimeTimeElement.
    2. Переделан пример с датой на пример с датой и временем.

--- examples/date_and_time.php
+++ examples/date_and_time.php
@@ -1,6 +1,7 @@
 <?php
 
 use HTMLElement\HTMLTextDateTimeDateElement;
+use HTMLElement\HTMLTextDateTimeTimeElement;
 
 /*
  * Подключаем элементы
@@ -9,6 +10,7 @@ include_once dirname(__FILE__).'/../HTMLElementBase.php';
 include_once dirname(__FILE__).'/../HTMLTextElement.php';
 include_once dirname(__FILE__).'/../HTMLTextDateTimeElement.php';
 include_once dirname(__FILE__).'/../HTMLTextDateTimeDateElement.php';
+include_once dirname(__FILE__).'/../HTMLTextDateTimeTimeElement.php';
 
 
 $date = new HTMLTextDateTimeDateElement();
@@ -16,15 +18,20 @@ $date->setFormat('d.m.Y');
 $date->setValue('30.03.2017');
 $date->setMin('01.01.2017');
 $date->setMax('31.01.2017');
-
-
 $date->check();
 
-
+$time = new HTMLTextDateTimeTimeElement();
+$time->setFormat('H:i:s');
+$time->setValue('12:00:05');
+$time->setMin('08:00:00');
+$time->setMax('18:00:00');
+$time->check();
 
 echo '<form method="POST">';
 
 echo $date->render();
+echo '<br /><br />';
+echo $time->render();
 echo '<br /><br />';
 echo '<input type="submit" >';
 
